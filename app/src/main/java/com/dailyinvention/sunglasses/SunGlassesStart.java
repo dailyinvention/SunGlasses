0c063a0a8654fccbe309a17a3786e5a460145e67
Stefan Holodnick <stefan@dailyinvention.com>
2014-10-04 22:13:39 -0400

    Added JSON request to Sunlight Foundation's Congress API.

--- app/src/main/java/com/dailyinvention/sunglasses/SunGlassesStart.java
+++ app/src/main/java/com/dailyinvention/sunglasses/SunGlassesStart.java
@@ -6,9 +6,18 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -31,9 +40,8 @@ public class SunGlassesStart extends Activity {
         longitude = String.valueOf(getLocation(context).getLongitude());
         location = "Latitude: " + latitude + "\r\n" + "Longitude: " + longitude;
 
-        setContentView(R.layout.activity_sunglasses);
-        TextView locationView = (TextView) findViewById(R.id.locationText);
-        locationView.setText(location);
+        new callSunlightAPI().execute(latitude, longitude);
+
     }
 
     public Location getLocation(Context context) {
@@ -93,6 +101,48 @@ public class SunGlassesStart extends Activity {
         }
         else {
             return null;
+
+        }
+    }
+
+    public class callSunlightAPI extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String urlString="https://congress.api.sunlightfoundation.com/legislators/locate?apikey=" + Globals.sunlightAPICode + "&latitude=" + params[0] + "&longitude=" + params[1]; // URL to call
+            String result;
+            String resultToDisplay = "";
+
+            InputStream in = null;
+
+            try {
+
+                //URL url = new URL(urlString);
+
+                HttpGet getResponse = new HttpGet(urlString);
+
+                ResponseHandler<String> responseHandler = new BasicResponseHandler();
+
+                HttpClient httpClient = new DefaultHttpClient();
+
+                result = httpClient.execute(getResponse,responseHandler);
+
+            } catch (Exception e ) {
+
+                System.out.println(e.getMessage());
+
+                result = e.getMessage();
+
+            }
+            return result;
+
+        }
+
+        protected void onPostExecute(String result) {
+            setContentView(R.layout.activity_sunglasses);
+            TextView locationView = (TextView) findViewById(R.id.locationText);
+            locationView.setText(location);
+            Log.i("JSON Result:",result);
 
         }
     }
